23ec6e5a348ad970602bd3414e39328d6e681ed8
Licheng-Wu <wlc.licheng@gmail.com>
2022-03-27 18:56:26 +0800

    use sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,32 +90,32 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Use SendGrid for Heroku
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.delivery_method = :smtp
-  # host = 'https://vast-brook-90140.herokuapp.com'
-  # config.action_mailer.default_url_options = { host: host }
-  # ActionMailer::Base.smtp_settings = {
-  #   :address        => 'smtp.sendgrid.net',
-  #   :port           => '587',
-  #   :authentication => :plain,
-  #   :user_name      => ENV['SENDGRID_USERNAME'],
-  #   :password       => ENV['SENDGRID_PASSWORD'],
-  #   :domain         => 'heroku.com',
-  #   :enable_starttls_auto => true
-  # }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://vast-brook-90140.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
 
   # Use MailGun for Heroku
-  host = 'vast-brook-90140.herokuapp.com'
-  config.action_mailer.default_url_options = {host: "#{host}"}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.smtp_settings = {
-    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
-    :password => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain => "vast-brook-90140.heroku.com",
-    :address => 'smtp.mailgun.org',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-    }
+  # host = 'vast-brook-90140.herokuapp.com'
+  # config.action_mailer.default_url_options = {host: "#{host}"}
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.smtp_settings = {
+  #   :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+  #   :password => ENV['MAILGUN_SMTP_PASSWORD'],
+  #   :domain => "vast-brook-90140.heroku.com",
+  #   :address => 'smtp.mailgun.org',
+  #   :port => 587,
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  #   }
 end
