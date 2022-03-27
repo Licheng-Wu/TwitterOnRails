525a62c67b4371187dd60e2ee316064d9a275584
Licheng-Wu <wlc.licheng@gmail.com>
2022-03-27 19:15:53 +0800

    reveal sendgrid details

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,8 +98,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => 'app254934816@heroku.com',
+    :password       => 'a13rq1om7056',
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
